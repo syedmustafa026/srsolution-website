e405c1a46ec53e9d17eaff49b70fa225c2da5b51
syedmustafa026 <syedmustafaahmed026@gmail.com>
2022-02-26 07:22:39 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//header
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbarzz");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
